f903106f9169d337f49531391b953b111da56e44
alvarezgabriel217 <alvarezgabriel217@gmail.com>
2026-01-20 21:58:15 -0500

    Fixed another issue in fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -5,8 +5,13 @@ import { Request, Response } from "express";
 
 export default (req: Request, res: Response) => {
   const num = req.params.num;
-
-  const fibN = fibonacci(parseInt(num, 10));
+  const n = parseInt(num, 10);
+  
+  if (!num || isNaN(n)) {
+	return res.status(400).send("Invalid number");
+  }
+  
+  const fibN = fibonacci(n);
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
